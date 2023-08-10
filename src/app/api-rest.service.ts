6af6e71da72138d36aaffb502dda9753aee3764a
Luciano de la Rubia <delarubialuciano@gmail.com>
2023-08-10 17:52:39 -0300

    test ngrok

--- src/app/api-rest.service.ts
+++ src/app/api-rest.service.ts
@@ -8,7 +8,9 @@ import { Observable } from 'rxjs';
 export class ApiRestService {
 
   // public url = 'http://localhost:4040'; // Cambia la URL según tu configuración
-  public url = 'http://explender.sytes.net:4040'; // Cambia la URL según tu configuración
+  // public url = 'http://explender.sytes.net:4040'; // Cambia la URL según tu configuración
+  // NgRok
+     public url = 'https://7cfe-2803-9800-b882-7eef-2533-22f6-3f21-6d97.ngrok-free.app'
 
   constructor(private http: HttpClient) { }
 
